606c4be7fec0a5303d053282545bbd7fc8fe0c9b
Đỗ Mạnh Quang <45645553+DoManhQuang@users.noreply.github.com>
2024-07-27 16:27:21 +0700

    workflow kafka

--- bigdata/sic-consumer.py
+++ bigdata/sic-consumer.py
@@ -1,7 +1,36 @@
-from kafka import KafkaConsumer, TopicPartition
-consumer = KafkaConsumer(bootstrap_servers="localhost:9092")
-consumer.subscribe('sic-kafka')
+from kafka import KafkaConsumer, KafkaProducer
+import pickle
+import json
+import ast
+
+consumer = KafkaConsumer(bootstrap_servers="localhost:9092", auto_offset_reset='latest')
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+consumer.subscribe(['sic-broker-input'])
+
+
+# Load
+with open('svm_model.pkl', 'rb') as f:
+    svm_clf = pickle.load(f)
 
 # stream consumer
 for msg in consumer:
-    print(msg.value)
+    msg_data = msg.value.decode("utf-8") # convert bytes to str
+
+    json_data = json.loads(msg_data) # convert str to json(dict)
+
+    print("Json >>> ", json_data, type(json_data))
+
+    y_pred = svm_clf.predict([json_data['record']])
+
+    print("y_pred >>>> ", y_pred)
+
+    msg_output = {
+        "uuid": json_data['uuid'],
+        "predict": str(y_pred[0])
+    }
+    
+    print("output >>> ", msg_output)
+
+    producer.send("sic-broker-output", msg_output)
+
+    print("Push data to topic sic-broker-output Done!")
--- bigdata/sic-producer.py
+++ bigdata/sic-producer.py
@@ -1,10 +1,34 @@
 # producer
 from kafka import KafkaProducer
 import time
+from tqdm import tqdm
+import pandas as pd
+import uuid
+import json
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-for i in range(20):
-    msg = f"message {i} : Xin Chao, Toi la HaUIer"
-    producer.send("sic-kafka", bytes(msg, 'utf-8'))
-    time.sleep(10)
+
+cols = ['Administrative', 'Administrative_Duration', 'Informational',
+       'Informational_Duration', 'ProductRelated', 'ProductRelated_Duration',
+       'BounceRates', 'ExitRates', 'PageValues', 'SpecialDay',
+       'OperatingSystems', 'Browser', 'Region', 'TrafficType', 'Revenue']
+
+df = pd.read_csv("/home/quang.domanh/kafka_2.13-3.7.1/online_shoppers_intention.csv", usecols=cols)
+input_data = df.head(100).values[:, : 14]
+print("INPUT : ", input_data.shape)
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+for _, record in tqdm(enumerate(input_data)):
+    
+    msg = {
+        "uuid": str(uuid.uuid1()),
+        "record": list(record),
+    }
+
+    # print(msg)
+
+
+    producer.send("sic-broker-input", msg)
+    time.sleep(20)
+
